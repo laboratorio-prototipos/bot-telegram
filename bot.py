0d85fec4bcbbf5dd92b028a61fd8a75a7cee34f1
Juan Molero <juanmolerog@gmail.com>
2016-03-22 02:19:05 +0000

    Not workign yet

--- bot.py
+++ bot.py
@@ -1,35 +1,43 @@
 from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
+from sqlalchemy.ext.declarative import declarative_base
 import telebot, random
 
 DEBUG = True
 
 engine = create_engine("sqlite:///users.db")
-metadata = MetaData()
-users = Table(users,metadata,
-        Column('user_id',Integer,primary_key=True),
-        Column('total_cachamas',Integer,default=0),
-        Column('last_request',DateTime))
+Base = declarative_base()
+
+class User(Base):
+    __tablename__ = 'User'
+    id = Column(Integer,nullable=False,primary_key=True)
+    total = Column(Integer,default=0,nullable=False)
+    last_generate = Column(DateTime,nullable=False)
+
+Base.metadata.create_all(engine)
 
 with open("token.txt") as f:
     token = f.read()
     bot = telebot.TeleBot(token)
 
-@bot.message_handler(commands=['start']):
-    def startMessage(message):
-        bot.send_message(message.chat,"Soy el bot de laboratorio de prototipos")
+@bot.message_handler(commands=['start'])
+def startMessage(message):
+    bot.send_message(message.chat.id,"Soy el bot de laboratorio de prototipos")
 
-@bot.message_handler(commands=['cachama']):
-    def cachama(message):
-        if (message.chat.type == 'private' or message.chat.type == 'channel') and DEBUG == False:
-            return
-        total_cachamas = get_users_cachamas(message.from.id)
-        if can_get_cachama(message.user.id):
-            new_cachamas = generate_new_cachamas()
-            total_cachamas=total_cachamas+new_cachamas
-            response = random.choice(cachama_templates_new).format(name=message.from.first_name,new=new_cachamas,total=total_cachamas)
-        else:
-            response = random.choice(cachama_templates).format(name=message.from.first_name,total=total_cachamas)
-        bot.send_message(message.chat,response)
+@bot.message_handler(commands=['cachama'])
+def cachama(message):
+    if (message.chat.type == 'private' or message.chat.type == 'channel') and DEBUG == False:
+        return
+    total_cachamas = get_users_cachamas(message.from_user.id)
+    if can_get_cachama(message.user.id):
+        new_cachamas = generate_new_cachamas()
+        total_cachamas=total_cachamas+new_cachamas
+        response = random.choice(cachama_templates_new).format(name=message.from_user.first_name,new=new_cachamas,total=total_cachamas)
+    else:
+        response = random.choice(cachama_templates).format(name=message.from_user.first_name,total=total_cachamas)
+    bot.send_message(message.chat.id,response)
+
+def can_get_cachama(id):
+    pass
 
 def generate_new_cachamas():
     return (10*random.randint(0,9))+random.randint(0,9)
