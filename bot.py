a18d13ef9f2c439542e1389eb962caa74fd4f006
Juan Molero <juanmolerog@gmail.com>
2016-03-22 18:09:30 +0000

    Version Inicial que funciona

--- bot.py
+++ bot.py
@@ -1,17 +1,26 @@
-from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime
+from sqlalchemy import create_engine, MetaData, Table, Column, Integer, String, DateTime, ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-import telebot, random
+from sqlalchemy.orm import relationship, sessionmaker, scoped_session
+from cachama_templates_new import cachama_templates_new as cachama_templates_new
+from cachama_templates import cachama_templates
+import telebot, random, datetime, time
 
 DEBUG = True
-
-engine = create_engine("sqlite:///users.db")
+LAST_CALL_PERIOD = 1*3600
+engine = create_engine("sqlite:///users.db",echo=True)
+Session = scoped_session(sessionmaker(bind=engine))
 Base = declarative_base()
 
 class User(Base):
-    __tablename__ = 'User'
+    __tablename__ = 'user'
     id = Column(Integer,nullable=False,primary_key=True)
-    total = Column(Integer,default=0,nullable=False)
-    last_generate = Column(DateTime,nullable=False)
+    cachamas = relationship("Cachama",backref="user",uselist=False)
+    
+class Cachama(Base):
+    __tablename__ = 'cachama'
+    id = Column(Integer,ForeignKey("user.id"),nullable=False,primary_key=True)
+    total = Column(Integer,default=0)
+    last_call= Column(DateTime)
 
 Base.metadata.create_all(engine)
 
@@ -23,21 +32,46 @@ with open("token.txt") as f:
 def startMessage(message):
     bot.send_message(message.chat.id,"Soy el bot de laboratorio de prototipos")
 
-@bot.message_handler(commands=['cachama'])
+@bot.message_handler(commands=['cachama','cachamas'])
 def cachama(message):
+    session = Session()
     if (message.chat.type == 'private' or message.chat.type == 'channel') and DEBUG == False:
         return
-    total_cachamas = get_users_cachamas(message.from_user.id)
-    if can_get_cachama(message.user.id):
+    user = session.query(User).filter(User.id == message.from_user.id).one_or_none()
+    if user is None:
+        user = User(id=message.from_user.id)
+        session.add(user)
+        cachama = Cachama(id=message.from_user.id,total=0,last_call=datetime.datetime.utcfromtimestamp(time.time()-(LAST_CALL_PERIOD+1)))
+        session.add(cachama)
+        session.flush()
+    delta = datetime.datetime.now() - user.cachamas.last_call
+    if delta.seconds > LAST_CALL_PERIOD:
         new_cachamas = generate_new_cachamas()
-        total_cachamas=total_cachamas+new_cachamas
-        response = random.choice(cachama_templates_new).format(name=message.from_user.first_name,new=new_cachamas,total=total_cachamas)
+        user.cachamas.total = user.cachamas.total+new_cachamas
+        user.cachamas.last_call = datetime.datetime.now()
+        response = random.choice(cachama_templates_new).format(name=message.from_user.first_name,new=new_cachamas,total=user.cachamas.total)
+        session.commit()
     else:
-        response = random.choice(cachama_templates).format(name=message.from_user.first_name,total=total_cachamas)
+        response = random.choice(cachama_templates).format(name=message.from_user.first_name,total=user.cachamas.total)
+        pass
+    Session.remove()
     bot.send_message(message.chat.id,response)
 
-def can_get_cachama(id):
-    pass
+def can_get_cachama(id_query):
+    user = session.query(User).filter(User.id == id_query).one_or_none()
+    if user is None:
+        new_user(id)
+    else:
+	delta = datetime.datetime.now() - user.last_call
+        if delta.seconds < (MAX_TIME):
+            return False
+        else:
+            return True
+
+def new_user(id_new):
+    user = User(id=id_new)
+    session.add(user)
+    session.commit()
 
 def generate_new_cachamas():
     return (10*random.randint(0,9))+random.randint(0,9)
---
+++ cachama_templates_new.py
@@ -0,0 +1,5 @@
+cachama_templates_new = [
+    "Fans le han traido a {name}, {new} cachamas para un total de {total}",
+    "Al laboratorio llegaron {new} cachamas con un papel que decia {name}, totalizando {total} cachamas",
+    "Ocho trajo {new} cachamas a {name}, chata tiene ahora {total} almuerzos resueltos"
+    ]
